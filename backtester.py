408f3ebf06c2613c8f27aeca33a47fbd6024d1d6
bp <bp@bpsite.xyz>
2025-07-31 23:43:59 -0700

    feat: Basic backtester website with inputs

--- backtester.py
+++ backtester.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import os
 import pandas as pd
 
-def backtest(tick1: str, tick2: str, win: str, buff: float):
+def backtest(tick1: str, tick2: str, win: str, buy_buff: float, sell_buff: float):
     print(f"BACKTEST FOR {tick1} & {tick2}")
     if not os.path.exists(f"{tick1}_2024.csv"):
         yf.download(f"{tick1}", start="2014-01-01", end="2025-07-01", auto_adjust=False, multi_level_index=False).to_csv(f'{tick1}_2024.csv', index=True)
@@ -14,7 +14,6 @@ def backtest(tick1: str, tick2: str, win: str, buff: float):
     money = 0
     shares = 0
     latestcloseprice = 0
-    buffer = buff
     commission_rate = 0.001
     slippage_rate = 0.0005
     holdshares = 100/df.head(n=1)["Adj Close"].item()
@@ -31,8 +30,10 @@ def backtest(tick1: str, tick2: str, win: str, buff: float):
     totalloss = 1
     profitfactor = 0
     indexnum = 0
+    stratarr = []
+    holdarr = []
     for row, row2 in zip(df.itertuples(), df2.itertuples()):
-        if (row._1 > row.SMA*(1+buffer)):
+        if (row._1 > row.SMA*(1+buy_buff)):
             if (money > 0):
                 adjusted_price = row2._1 * (1 + slippage_rate)
                 trade_value = money * (1 - commission_rate)
@@ -40,7 +41,7 @@ def backtest(tick1: str, tick2: str, win: str, buff: float):
                 prevportval = money
                 money = 0
                 trades += 1
-        elif (row._1 < row.SMA*(1-buffer)):
+        elif (row._1 < row.SMA*(1-sell_buff)):
             if (shares > 0):
                 adjusted_price = row2._1 * (1 - slippage_rate)
                 trade_value = shares * adjusted_price * (1 - commission_rate)
@@ -55,22 +56,23 @@ def backtest(tick1: str, tick2: str, win: str, buff: float):
                 trades += 1
         latestcloseprice = row2._1 
         portval = money + shares * latestcloseprice
+        holdportval = holdshares * row._1
+        holdarr.insert(indexnum, holdportval)
+        stratarr.insert(indexnum, portval)
+        indexnum += 1
     portreturn = (portval-100)/100
     CAGR = ((portreturn) ** (1/11.5) - 1)*100
     totalportval += portval
     totalportreturn += portreturn
     totalcagr += CAGR
     expectancy = ((wintrades/trades) * (totalprofit/trades)) - ((1-(wintrades/trades))* (totalloss/trades))
-    print(f'Portfolio value is ${portval:.2f}, buffer: {buffer:.3f}, CAGR: {CAGR:.2f}%, Trades: {trades}, Return: {portreturn*100:.2f}%')
+    print(f'Portfolio value is ${portval:.2f}, Buy buffer: {buy_buff:.3f}, Sell buffer: {sell_buff:.3f}, CAGR: {CAGR:.2f}%, Trades: {trades}, Return: {portreturn*100:.2f}%')
     print(f'Win rate is ${100*(wintrades/trades):.2f}%, expectancy: {expectancy:.2f}, profitfactor: {totalprofit/totalloss:.2f}')
     print('------')
     holdportval = holdshares * df.tail(n=1)["Adj Close"].item()
     print(f'The average portfolio value is ${totalportval/10:.2f}, CAGR: {totalcagr/10:.2f}%, Return: {totalportreturn*10:.2f}%')
-    print(f'Holding would have gave Portfolio value: ${holdportval: .2f}, CAGR: {((holdportval/ 100) ** (1/11.5) - 1)*100}%, Return: {(holdportval-100):.2f}%')
-
-def main():
-    while True:
-        backtest(input("Enter First Ticker: "), input("Enter Second Ticker: "), input("SMA Window (ex: 200d): "))
+    print(f'Holding would have gave Portfolio value: ${holdportval: .2f}, CAGR: {((holdportval/ 100) ** (1/11.5) - 1)*100:.2f}%, Return: {(holdportval-100):.2f}%')
+    return [stratarr, holdarr]
 
 if __name__ == "__main__":
-    main()
+    backtest("QQQ", "TQQQ", "200d", 0.02)
--- web.py
+++ web.py
@@ -1,25 +1,41 @@
 import dash
+from backtester import backtest
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-def load_d(ticker: str, ticker2: str):
-    data1 = pd.read_csv(f"{ticker}_2024.csv", parse_dates=True, index_col=0)
-    data2 = pd.read_csv(f"{ticker2}_2024.csv", parse_dates=True, index_col=0)
-    data1["Datetime"] = pd.to_datetime(data1.index)
-    data2["Datetime"] = pd.to_datetime(data2.index)
-    data = pd.merge(data1, data2, on="Datetime")
-    options = ["Adj Close", "Close", "High", "Low", "Open", "Volume"]
-    for option in options:
-        data[f"{option} {ticker}"] = data[f"{option}_x"]
-        data[f"{option} {ticker2}"] = data[f"{option}_y"]
-    return data 
+app = dash.Dash(__name__)
+
+def load_d(ticker: str, ticker2: str, buybuff: str, sellbuff: str):
+    d1 = pd.read_csv(f"{ticker}_2024.csv", parse_dates=True, index_col=0)
+    d1["Datetime"] = pd.to_datetime(d1.index)
+    data = backtest(ticker, ticker2, "200d", float(buybuff)*0.01, float(sellbuff)*0.01)
+    frame1 = pd.DataFrame(data[0], columns=["Strategy"], index=d1["Datetime"])
+    frame2 = pd.DataFrame(data[1], columns=["Hold"], index=d1["Datetime"])
+    frame1["Date"] = pd.to_datetime(frame1.index)
+    frame2["Date"]= pd.to_datetime(frame2.index)
+    print(frame1)
+    print(frame2)
+    frame = pd.merge(frame1, frame2, on="Date")
+    return frame 
 
 app.layout = html.Div([
     html.Div(children="test"),
     dcc.Dropdown(id="newtick", options=["QQQ", "QLD", "TQQQ", "SPY", "SSO", "UPRO"], value="QQQ", placeholder="QQQ"),
     dcc.Dropdown(id="newtick2", options=["QQQ", "QLD", "TQQQ", "SPY", "SSO", "UPRO"], value="TQQQ", placeholder="TQQQ"),
     dcc.Dropdown(id="newY", options=["Adj Close", "Close", "High", "Low", "Open", "Volume"], value="Adj Close", placeholder="Adj Close"),
+    dcc.Input(
+        id="buybuffer-input",
+        type="number",
+        value="2",
+        placeholder="2"
+    ),
+    dcc.Input(
+        id="sellbuffer-input",
+        type="number",
+        value="2",
+        placeholder="2"
+    ),
     #dcc.Graph(animate=True, id="graph1", animation_options={ "frame" : { "redraw" : True}, "transition": {"duration": 5, "ease": "linear"}}),
     dcc.Graph(figure={}, id="graph1"),
 ])
@@ -28,12 +44,12 @@ app.layout = html.Div([
     Output('graph1', 'figure'),
     Input('newtick', 'value'),
     Input('newtick2', 'value'),
-    Input('newY', 'value')
+    Input('buybuffer-input', 'value'),
+    Input('sellbuffer-input', 'value')
 )
-def update_ticker(ticker: str, ticker2: str, newYval: str):
-    fig = px.line(data_frame=load_d(ticker, ticker2), x="Datetime", y=[f"{newYval} {ticker}", f"{newYval} {ticker2}"])
+def update_ticker(ticker: str, ticker2: str, buybuff: str, sellbuff: str):
+    fig = px.line(data_frame=load_d(ticker, ticker2, buybuff, sellbuff), x="Date", y=["Strategy", "Hold"])
     return fig
 
 if __name__ == "__main__":
-    app = dash.Dash(__name__)
     app.run()
